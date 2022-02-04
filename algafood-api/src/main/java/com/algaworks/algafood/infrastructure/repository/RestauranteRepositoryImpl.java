31b2a9b0cd947a8477ee0ab70b267b980eb79cc2
Ricardo Tavares da Silva <ricardotavaresjc@gmail.com>
2022-02-03 21:33:35 -0300

    5.12. Implementando uma consulta dinâmica com JPQL

--- algafood-api/src/main/java/com/algaworks/algafood/infrastructure/repository/RestauranteRepositoryImpl.java
+++ algafood-api/src/main/java/com/algaworks/algafood/infrastructure/repository/RestauranteRepositoryImpl.java
@@ -1,12 +1,15 @@
 package com.algaworks.algafood.infrastructure.repository;
 
 import java.math.BigDecimal;
+import java.util.HashMap;
 import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
 
 import com.algaworks.algafood.domain.model.Restaurante;
 import com.algaworks.algafood.domain.repository.RestauranteRepositoryQueries;
@@ -17,20 +20,57 @@ public class RestauranteRepositoryImpl implements RestauranteRepositoryQueries {
 	@PersistenceContext
 	private EntityManager manager;
 	
+	//Implementação consulta dinamica
 	@Override
 	public List<Restaurante> find(String nome,
 			BigDecimal taxaFreteInicial, BigDecimal taxaFreteFinal){
 		
-		var jqpl = "from Restaurante where nome like :nome "
-				+ "and taxaFrete between :taxaInicial and :taxaFinal";
+//		var jqpl = "from Restaurante where nome like :nome "
+//				+ "and taxaFrete between :taxaInicial and :taxaFinal";
 		
-		//não permite colocar % direto na string com a instrução jqpl
-		return manager.createQuery(jqpl, Restaurante.class)
-				.setParameter("nome", "%" + nome + "%")
-				.setParameter("taxaInicial", taxaFreteInicial)
-				.setParameter("taxaFinal", taxaFreteFinal)
-				.getResultList();
+		StringBuilder jqpl = new StringBuilder("from Restaurante where 0 = 0 ");
+		
+		//usado para que a parte de settar os parametros fique dinamico tambem
+		var parametros = new HashMap<String, Object>();
+		
+		//O nome pode vir vazio, então podemos usar a classe StringUtils do pacote org.springframework.util para pegar tanto nulo como vazio 
+		if(StringUtils.hasLength(nome)) {
+			jqpl.append("and nome like :nome ");
+			//não permite colocar % direto na string com a instrução jqpl
+			parametros.put("nome", "%" + nome + "%");
+		}
+		
+		if(taxaFreteInicial != null) {
+			jqpl.append("and taxaFrete >= :taxaInicial ");
+			parametros.put("taxaInicial", taxaFreteInicial);
+		}
+		
+		if(taxaFreteFinal != null) {
+			jqpl.append("and taxaFrete < :taxaFinal ");
+			parametros.put("taxaFinal", taxaFreteFinal);
+		}
+		
+		TypedQuery<Restaurante> query = manager.createQuery(jqpl.toString(), Restaurante.class);
+		
+		parametros.forEach((chave, valor) -> query.setParameter(chave, valor));
+		
+		return query.getResultList();
 	}
 	
+	//Implementação rigida
+//	@Override
+//	public List<Restaurante> find(String nome,
+//			BigDecimal taxaFreteInicial, BigDecimal taxaFreteFinal){
+//		
+//		var jqpl = "from Restaurante where nome like :nome "
+//				+ "and taxaFrete between :taxaInicial and :taxaFinal";
+//		
+//		//não permite colocar % direto na string com a instrução jqpl
+//		return manager.createQuery(jqpl, Restaurante.class)
+//				.setParameter("nome", "%" + nome + "%")
+//				.setParameter("taxaInicial", taxaFreteInicial)
+//				.setParameter("taxaFinal", taxaFreteFinal)
+//				.getResultList();
+//	}
 	
 }
